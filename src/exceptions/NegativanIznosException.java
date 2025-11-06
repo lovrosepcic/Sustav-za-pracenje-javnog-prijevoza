e5b454baf944bc20ae7a617bcae2e884d8429450
msepc <lovrosepcic@gmail.com>
2025-11-06 20:01:52 +0100

    test

---
+++ src/exceptions/NegativanIznosException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NegativanIznosException extends RuntimeException {
+    public NegativanIznosException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/TimeoutException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class TimeoutException extends RuntimeException {
+    public TimeoutException(String message) {
+        super(message);
+    }
+}
